095855fe9ede8da7226b718ee75fd34aa2c83572
Robert Böhnke <robb@robb.is>
2013-02-02 20:34:35 +0100

    Merging spec files

--- Specs/StubbilinoSpec.m
+++ Specs/StubbilinoSpec.m
@@ -18,4 +18,45 @@ describe(@"Stubbilino", ^{
     });
 });
 
+describe(@"A stubbed object", ^{
+    __block id originalObject;
+    __block id<SBStub> stubbedObject;
+
+    beforeEach(^{
+        originalObject = [[NSObject alloc] init];
+        stubbedObject = [Stubbilino stubObject:originalObject];
+    });
+
+    it(@"is identical to the original object", ^{
+        expect(stubbedObject).to.beIdenticalTo(originalObject);
+    });
+
+    it(@"can stub methods", ^{
+        [stubbedObject stubMethod:@selector(description)
+                        withBlock:^{ return @"stubbed"; }];
+
+        expect(stubbedObject.description).to.equal(@"stubbed");
+    });
+
+    it(@"can remove stubbed methods", ^{
+        NSString *originalDescription = [originalObject description];
+
+        [stubbedObject stubMethod:@selector(description)
+                        withBlock:^{ return @"stubbed"; }];
+
+        [stubbedObject removeStub:@selector(description)];
+
+        expect(stubbedObject.description).to.equal(originalDescription);
+    });
+
+    it(@"does not affect other objects", ^{
+        id otherObject = [[NSObject alloc] init];
+
+        [stubbedObject stubMethod:@selector(description)
+                        withBlock:^{ return @"stubbed"; }];
+
+        expect([otherObject description]).toNot.equal(@"stubbed");
+    });
+});
+
 SpecEnd
